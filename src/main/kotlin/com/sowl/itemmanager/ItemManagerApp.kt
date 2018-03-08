e7fe3679b8b64154e313ef8d88c08ea97eeb093a
sukso96100 <sukso96100@gmail.com>
2018-03-08 13:58:58 +0900

    Added Skeleton files

---
+++ src/main/kotlin/com/sowl/itemmanager/ItemManagerApp.kt
@@ -0,0 +1,20 @@
+package com.sowl.itemmanager
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("My Example Blog2", ContentType.Text.Html)
+        }
+    }
+}
+
